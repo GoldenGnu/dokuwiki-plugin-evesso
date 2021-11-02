b96edb78ba9a5f844da8f6f69fa8c42373d5a62a
GoldenGnu <nkr@niklaskr.dk>
2021-11-02 19:24:50 +0100

    Removed use of SSO /oauth/verify (Deprecated)
    Use JWT instead (Original JWT code by Steve Ronuken)

--- classes/EveOnlineAdapter.php
+++ classes/EveOnlineAdapter.php
@@ -30,19 +30,27 @@ class EveOnlineAdapter extends AbstractAdapter {
         $http = new \DokuHTTPClient();
         $data = array();
 
-        /** var OAuth\OAuth2\Service\Generic $this->oAuth */
-        $result = json_decode($this->oAuth->request('/oauth/verify'), true);
+        //Get access_token
+        $access_token = $this->oAuth->getStorage()->retrieveAccessToken($this->oAuth->service())->getAccessToken();
 
-        $data['user'] = $result['CharacterName'];
-        $data['name'] = $result['CharacterName'];
-        $data['mail'] = $result['CharacterID'] . '@eveonline.com';
+        //Split JWT into 3 parts > Take the 2nd part (payload) > base64 decode it > json decode it
+        $jwt_payload = json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $access_token)[1]))));
 
-        if (!isset($result['CharacterID'])) {
+        //Get character name and id
+        $character_name=$jwt_payload->name; //Character Name
+        $character_id=explode(":",$jwt_payload->sub)[2]; //Charater ID (remove the extra stuff)
+
+        if (!isset($character_id)) {
             return $data;
         }
 
+        //Set character name and id
+        $data['user'] = $character_name;
+        $data['name'] = $character_name;
+        $data['mail'] = $character_id . '@eveonline.com';
+
         //Get character corporation, alliance, and faction
-        $affiliation_post = $http->post('https://esi.evetech.net/latest/characters/affiliation/?datasource=tranquility', '[' . $result['CharacterID'] . ']');
+        $affiliation_post = $http->post('https://esi.evetech.net/latest/characters/affiliation/?datasource=tranquility', '[' . $character_id . ']');
         if ($affiliation_post === false) {
             return $data;
         }
@@ -59,6 +67,7 @@ class EveOnlineAdapter extends AbstractAdapter {
             $ids[] = $entry['corporation_id'];
         }
 
+        //Convert ids to names
         $names_post = $http->post('https://esi.evetech.net/latest/universe/names/?datasource=tranquility', '[' . implode(",", $ids) . ']');
         if ($names_post === false) {
             return $data;
