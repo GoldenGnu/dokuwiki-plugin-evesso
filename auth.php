cf37781322443cb034b99de3b6d2823b533cf6af
GoldenGnu <nkr@niklaskr.dk>
2022-09-08 23:17:00 +0200

    Removed unused code

--- auth.php
+++ auth.php
@@ -22,24 +22,6 @@ class auth_plugin_evesso extends auth_plugin_authplain {
         $this->cando['external'] = true;
     }
 
-    private function handleState($state) {
-        /** @var \helper_plugin_farmer $farmer */
-        $farmer = plugin_load('helper', 'farmer', false, true);
-        $data = json_decode(base64_decode(urldecode($state)));
-        if (empty($data->animal) || $farmer->getAnimal() == $data->animal) {
-            return;
-        }
-        $animal = $data->animal;
-        $allAnimals = $farmer->getAllAnimals();
-        if (!in_array($animal, $allAnimals)) {
-            msg('Animal ' . $animal . ' does not exist!', -1);
-            return;
-        }
-        global $INPUT;
-        $url = $farmer->getAnimalURL($animal) . '/doku.php?' . $INPUT->server->str('QUERY_STRING');
-        send_redirect($url);
-    }
-
     /**
      * Handle the login
      *
@@ -53,10 +35,6 @@ class auth_plugin_evesso extends auth_plugin_authplain {
      */
     function trustExternal($user, $pass, $sticky = false) {
         global $USERINFO, $INPUT;
-
-        if ($INPUT->has('state') && plugin_load('helper', 'farmer', false, true)) {
-            $this->handleState($INPUT->str('state'));
-        }
 
         // check session for existing oAuth login data
         $session = $_SESSION[DOKU_COOKIE]['auth'];
